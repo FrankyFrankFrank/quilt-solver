957c7259ef1cda84cc117659dcfa4a173087fcdd
FrankyFrankFrank <adam@webadamfrank.com>
2022-08-01 14:03:33 -0400

    generates columns and rows

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,10 @@
 export const generateSquares = () => {
     const squares = []
     for (let i = 0; i < 256; i++) {
-        squares.push(i * i)
+        squares.push({
+            column: i % 16,
+            row: Math.floor(i / 16),
+        })
     }
     return squares
 }
--- src/utils.test.js
+++ src/utils.test.js
@@ -6,4 +6,16 @@ describe('utils', () => {
         const squares = generateSquares()
         expect(squares.length).toBe(256)
     })
+
+    it('returns 16 squares on column 0', () => {
+        const squares = generateSquares()
+        const squaresOnColumn1 = squares.filter((square) => square.column === 0)
+        expect(squaresOnColumn1.length).toBe(16)
+    })
+
+    it('returns 16 squares on row 0', () => {
+        const squares = generateSquares()
+        const squaresOnRow1 = squares.filter((square) => square.row === 0)
+        expect(squaresOnRow1.length).toBe(16)
+    })
 })

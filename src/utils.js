04e537cfda0e510055f493b5e694da6d00b2432b
FrankyFrankFrank <adam@webadamfrank.com>
2022-08-01 14:12:14 -0400

    generates columns rows and colors

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,16 @@
 export const generateSquares = () => {
     const squares = []
+    const colors = [
+        'red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'forestgreen'
+    ]
+
     for (let i = 0; i < 256; i++) {
         squares.push({
             column: i % 16,
             row: Math.floor(i / 16),
+            color: colors[i % 8]
         })
     }
+    
     return squares
 }
--- src/utils.test.js
+++ src/utils.test.js
@@ -7,15 +7,27 @@ describe('utils', () => {
         expect(squares.length).toBe(256)
     })
 
-    it('returns 16 squares on column 0', () => {
+    it.each([
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+    ])('returns 16 squares on column %i', (column) => {
         const squares = generateSquares()
-        const squaresOnColumn1 = squares.filter((square) => square.column === 0)
-        expect(squaresOnColumn1.length).toBe(16)
+        const squaresOnColumn = squares.filter((square) => square.column === column)
+        expect(squaresOnColumn.length).toBe(16)
     })
 
-    it('returns 16 squares on row 0', () => {
+    it.each([
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+    ])('returns 16 squares on row %i', (row) => {
         const squares = generateSquares()
-        const squaresOnRow1 = squares.filter((square) => square.row === 0)
-        expect(squaresOnRow1.length).toBe(16)
+        const squaresOnRow = squares.filter((square) => square.row === row)
+        expect(squaresOnRow.length).toBe(16)
+    })
+
+    it.each([
+        'red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'forestgreen'
+    ])('returns 32 squares of each color', (color) => {
+        const squares = generateSquares()
+        const squaresOfColor = squares.filter((square) => square.color === color)
+        expect(squaresOfColor.length).toBe(32)
     })
 })

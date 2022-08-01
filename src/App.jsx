c8889af816870b376487ea216e9c355f00fcb27b
FrankyFrankFrank <adam@webadamfrank.com>
2022-08-01 16:27:20 -0400

    adds white squares

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import { generateSquares } from './utils'
 function App() {
   const squares = generateSquares()
 
+  const getRotation = (index) => {
+    const rotations = [0, 90, 180, 270]
+
+    // get a random rotation
+    const rotation = rotations[Math.floor(Math.random() * rotations.length)]
+    return rotation
+  }
+
   return (
     <div id="grid">
       {squares.map((square, index) => (
@@ -13,7 +21,7 @@ function App() {
           className="square"
           style={{ backgroundColor: square.color }}
         >
-          <svg width="38" height="38" viewBox="0 0 100 100">
+          <svg width="38" height="38" viewBox="0 0 100 100" style={{ transform: `rotate(${getRotation()}deg)`}}>
             <polygon points="0 0, 100 0, 100 100" fill='white' />
           </svg>
         </div>
